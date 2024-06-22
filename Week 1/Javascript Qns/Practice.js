ac89cbf3ddf3733d368bc93d6bbb29557c60803b
TarunRaveesh <tarunraveesh07@gmail.com>
2024-06-22 19:20:07 +0530

    Commit

--- Week 1/Javascript Qns/Practice.js
+++ Week 1/Javascript Qns/Practice.js
@@ -2,7 +2,7 @@ let users = [
     { name: "Tarun", age: 20, city: "Delhi" },
     { name: "Rahul", age: 21, city: "Mumbai" },
     { name: "Sonam", age: 22, city: "Bangalore" },
-    { name: "Rohit", age: 23, city: "Chennai" },
+    { name: "Somesh", age: 23, city: "Chennai" },
 ]
 
 console.log(users[0].city)
--- Week 1/Javascript Qns/SingleThreaded/count.js
+++ Week 1/Javascript Qns/SingleThreaded/count.js
@@ -3,6 +3,13 @@ const startTime = Date.now();
 let count = 0;
 let sum = 0;
 
+
+function printTime() {
+    console.log('2 secons have passed');
+}
+
+setTimeout(printTime, 2000);
+
 for (let i = 1; i <= 10000000000; i++) {
     count++;
     sum += i;
